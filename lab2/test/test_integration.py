62562f26c8b34320e3d2159b7cacd1d3e49fdd17
Alexander <smrdak152@gmail.com>
2025-11-14 00:56:54 +0600

    Работа с фикстурами

--- lab2/test/test_integration.py
+++ lab2/test/test_integration.py
@@ -1,52 +1,36 @@
-import pytest
-from testcontainers.postgres import PostgresContainer
-from testcontainers.kafka import KafkaContainer
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
 import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from orders.model import Base
+import time
 from orders.model import Order
 from orders.service import OrderService
 from notifications.service import NotificationService
 from orderFactory import OrderFactory
-import time
 
 
-def test():
-    with PostgresContainer("postgres:16") as postgres, KafkaContainer(
-        "confluentinc/cp-kafka:7.5.0"
-    ) as kafka:
-        kafka_bootstrap = kafka.get_bootstrap_server()
-        topic = "orders"
+def test_create_order(db_session, kafka_container):
+    topic = "orders"
+    kafka_bootstrap = kafka_container.get_bootstrap_server()
 
-        engine = create_engine(postgres.get_connection_url())
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
+    test_order = OrderFactory.build()
 
-        with Session() as session:
-            test_order = OrderFactory.build()
+    order_service = OrderService(db_session, kafka_bootstrap, topic)
+    created_order = order_service.create_order(
+        test_order.item_name, test_order.quantity
+    )
 
-            order_service = OrderService(session, kafka_bootstrap, topic)
-            time.sleep(5)
-            created_order = order_service.create_order(
-                test_order.item_name, test_order.quantity
-            )
+    db_order = db_session.get(Order, created_order.id)
+    assert db_order.item_name == test_order.item_name
+    assert db_order.quantity == test_order.quantity
 
-            db_order = session.get(Order, created_order.id)
-            assert db_order.item_name == test_order.item_name
-            assert db_order.quantity == test_order.quantity
+    notification_service = NotificationService(kafka_bootstrap, topic)
+    time.sleep(5)
+    message = notification_service.listen()
+    notification_service.close()
 
-            notification_service = NotificationService(kafka_bootstrap, topic)
-            time.sleep(5)
-            message = notification_service.listen()
-            notification_service.close()
-
-            assert message is not None
-            assert message["id"] == created_order.id
-            assert message["item_name"] == created_order.item_name
-            assert message["quantity"] == created_order.quantity
+    assert message is not None
+    assert message["id"] == created_order.id
+    assert message["item_name"] == created_order.item_name
+    assert message["quantity"] == created_order.quantity
