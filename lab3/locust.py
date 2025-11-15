32101040f2216eac6867ff33b97564c33d421022
Alexander <smrdak152@gmail.com>
2025-11-16 00:27:50 +0600

    Фикс Locust

--- lab3/locust.py
+++ lab3/locust.py
@@ -14,19 +14,19 @@ class BlogScenario(SequentialTaskSet):
             "title": "Обезьяний побег",
             "content": "14.11.2025 в 12:00 по Гринвичу из Новосибирского зоопарка сбежало три макаки. " 
                        "Их точное местоположение до сих пор неизвестно.",
-        })
+        }, name="post posts")
         
         if response.status_code == 201:
             self.post_id = response.json()["id"]
-            self.client.get(f"/posts/{self.post_id}")
+            self.client.get(f"/posts/{self.post_id}", name="get posts")
             
     @task(3)
     def read_all_posts(self):
-        self.client.get("/posts")
+        self.client.get("/posts", name="get posts")
 
     @task(5)
     def read_post_with_postid_1(self):
-        self.client.get(f"/posts/1")
+        self.client.get(f"/posts/1", name="get posts")
 
 
 class BlogUser(HttpUser):
